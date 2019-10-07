17496cf388f4a2eb5af42c5a197900d6606ff090
maryna-js <rastargueva.m@gmail.com>
2019-10-07 17:33:26 +0300

    feat: guessing game task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+      this.guessValue = Math.round((this.min+this.max)/2);
+       return this.guessValue;
     }
 
     lower() {
-
+        this.max =  this.guessValue;
     }
 
     greater() {
-
+        this.min = this.guessValue;
     }
 }
 
